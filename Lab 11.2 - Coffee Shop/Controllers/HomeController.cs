32ef09975427fcf199de66f5afd69400dbf4d721
MichaelGKing91 <michaelking@quickenloans.com>
2021-05-11 16:57:24 -0400

    first

---
+++ Lab 11.2 - Coffee Shop/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Lab_11._2___Coffee_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_11._2___Coffee_Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult adduser(string firstname, string lastname)
+        {
+            ViewData["firstname"] = firstname;
+            ViewData["lastname"] = lastname;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
